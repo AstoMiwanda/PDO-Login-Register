42e5daadbd32e917ae71df97c4ac25d15d0c1e83
AstoMiwanda <30406998+AstoMiwanda@users.noreply.github.com>
2017-10-14 21:37:17 +0700

    Create input.php

---
+++ input.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Regiter</title>
+</head>
+<body>
+
+<center><table style="margin-top: 50px;">
+<form action="view.php" method="post">
+	<tr>
+		<td>E-mail</td>
+		<td>:</td>
+		<td><input type="email" name="email" size="15"></td>
+	</tr>
+	<tr>
+		<td>Full Name</td>
+		<td>:</td>
+		<td><input required type="text" name="nama" size="25"></td>
+	</tr>
+	<tr>
+		<td>Username</td>
+		<td>:</td>
+		<td><input required  type="text" name="username" size="20"></td>
+	</tr>
+	<tr>
+		<td>Password</td>
+		<td>:</td>
+		<td><input required type="password" name="password" size="20"></td>
+	</tr>
+	<tr>
+		<td>&nbsp</td>
+		<td>&nbsp</td>
+		<td><input type="submit" value="Sing Up"></td>
+	</tr>
+</form>
+</table></center>
+
+</body>
+</html>

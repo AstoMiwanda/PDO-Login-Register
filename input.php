0973aaaf18789f51f62aaef9a976801a9285dcb6
AstoMiwanda <30406998+AstoMiwanda@users.noreply.github.com>
2017-10-17 21:19:35 +0700

    Update input.php

--- input.php
+++ input.php
@@ -2,15 +2,29 @@
 <html>
 <head>
 	<title>Form Regiter</title>
+<style type="text/css">
+	#login{
+		padding: 0;
+		margin: 0;
+	}
+	#login input[type="email"],
+	#login input[type="text"],
+	#login input[type="password"]{
+		padding: 10px;
+		margin: 5px;
+	}
+</style>
 </head>
 <body>
 
-<center><table style="margin-top: 50px;">
+<center>
+	<h1>Silahkan Login</h1>
+	<table style="margin-top: 50px;">
 <form action="view.php" method="post">
 	<tr>
 		<td>E-mail</td>
 		<td>:</td>
-		<td><input type="email" name="email" size="15"></td>
+		<td><input required type="email" name="email" size="15"></td>
 	</tr>
 	<tr>
 		<td>Full Name</td>
@@ -30,7 +44,7 @@
 	<tr>
 		<td>&nbsp</td>
 		<td>&nbsp</td>
-		<td><input type="submit" value="Sing Up"></td>
+		<td><input type="submit" value="Sing In"></td>
 	</tr>
 </form>
 </table></center>

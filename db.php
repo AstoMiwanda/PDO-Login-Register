fd44749276e47251adbe9f37fc536d2b817d6e2b
AstoMiwanda <30406998+AstoMiwanda@users.noreply.github.com>
2017-10-19 14:29:15 +0700

    Create db.php

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+
+	//Konfigurasi database
+	
+	$host = "localhost";
+	$dbname = "dbinstagram";
+	$username = "root";
+	$password = "";
+
+	try{
+
+		//Buat object PDO baru dan simpan ke variable $db
+
+		$db = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
+
+		//Mengatur error mode di PDO untuk segera menampilkan exception ketika ada kesalahan
+
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (PDOException $exception){
+		die("Connection error: ".$exception->getMessage());
+	}
+?>
